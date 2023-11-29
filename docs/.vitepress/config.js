01aab79ee9e0cf92158e5ab5ad50b37e208510de
chenhongtao <chenhongtao@sutpc.com>
2023-11-29 11:10:42 +0800

    feat:新增java学习笔记

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -12,216 +12,232 @@ export default defineConfig({
 		lastUpdatedText: "最近更新时间",
 		// 导航栏配置
 		nav: [
-			{ text: '每日笔记', link: '/notes/'},
-			{ text: '学习笔记', link: '/study/'},
+			{ text: '每日笔记', link: '/notes/' },
+			{ text: '学习笔记', link: '/study/' },
 		],
 		// 侧边栏配置
 		sidebar: {
 			'/study/': [{
-					text: 'vue2学习笔记',
-					items: [
-						{ text: '介绍', link: '/study/vue/vue2/introduction/README' },
-						{ text: '源码目录设计和架构设计', link: '/study/vue/vue2/design/README' },
-						{
-							text: 'Rollup构建版本',
-							items: [
-								{ text: 'Rollup基础知识', link: '/study/vue/vue2/rollup/README' },
-								{ text: 'Vue中的Rollup构建', link: '/study/vue/vue2/rollup/vue' },
-							],
-							collapsible: true,
-							collapsed: true
-						},
-						{
-							text: '从入口到构造函数整体流程',
-							items: [
-								{ text: '整体流程', link: '/study/vue/vue2/entry/README' },
-								{ text: 'initGlobalAPI', link: '/study/vue/vue2/entry/global' },
-								{ text: 'initMixin流程', link: '/study/vue/vue2/entry/init' },
-								{ text: 'stateMixin流程', link: '/study/vue/vue2/entry/state' },
-								{ text: 'eventsMixin流程', link: '/study/vue/vue2/entry/events' },
-								{ text: 'lifecycleMixin流程', link: '/study/vue/vue2/entry/lifecycle' },
-								{ text: 'renderMixin流程', link: '/study/vue/vue2/entry/render' },
-							],
-							collapsible: true,
-							collapsed: true
-						},
-						{
-							text: '响应式原理',
-							items: [
-								{ text: '介绍', link: '/study/vue/vue2/reactive/README' },
-								{ text: '前置核心概念', link: '/study/vue/vue2/reactive/prepare' },
-								{ text: 'props处理', link: '/study/vue/vue2/reactive/props' },
-								{ text: 'methods处理', link: '/study/vue/vue2/reactive/methods' },
-								{ text: 'data处理', link: '/study/vue/vue2/reactive/data' },
-								{ text: 'computed处理', link: '/study/vue/vue2/reactive/computed' },
-								{ text: 'watch处理', link: '/study/vue/vue2/reactive/watch' },
-								{ text: '深入响应式原理', link: '/study/vue/vue2/reactive/reactive' },
-								{ text: '依赖收集', link: '/study/vue/vue2/reactive/dep' },
-								{ text: '派发更新', link: '/study/vue/vue2/reactive/notify' },
-								{ text: 'nextTick实现原理', link: '/study/vue/vue2/reactive/nexttick' },
-								{ text: '变化侦测注意事项', link: '/study/vue/vue2/reactive/problem' },
-								{ text: '变化侦测API实现', link: '/study/vue/vue2/reactive/api' },
-							],
-							collapsible: true,
-							collapsed: true
-						},
-						{
-							text: '虚拟DOM和VNode',
-							items: [
-								{ text: '虚拟DOM', link: '/study/vue/vue2/dom/README' },
-								{ text: 'VNode介绍', link: '/study/vue/vue2/dom/vnode' },
-								{ text: 'Diff算法', link: '/study/vue/vue2/dom/diff' },
-							],
-							collapsible: true,
-							collapsed: true
-						},
-						{
-							text: '组件化',
-							items: [
-								{ text: '介绍', link: '/study/vue/vue2/component/README' },
-								{ text: '$mount方法', link: '/study/vue/vue2/component/mount' },
-								{ text: 'render和renderProxy', link: '/study/vue/vue2/component/render' },
-								{ text: 'createElement', link: '/study/vue/vue2/component/createElement' },
-								{ text: 'createComponent', link: '/study/vue/vue2/component/createComponent' },
-								{ text: '合并策略', link: '/study/vue/vue2/component/merge' },
-								{ text: 'update和patch', link: '/study/vue/vue2/component/patch' },
-								{ text: '组件生命周期', link: '/study/vue/vue2/component/lifecycle' },
-								{ text: '组件注册', link: '/study/vue/vue2/component/register' },
-							],
-							collapsible: true,
-							collapsed: true
-						},
-						{
-							text: '编译原理',
-							items: [
-								{ text: '介绍', link: '/study/vue/vue2/compile/README' },
-								{ text: 'compileToFunctions', link: '/study/vue/vue2/compile/compileToFunctions' },
-								{ text: 'parse模板解析', link: '/study/vue/vue2/compile/parse' },
-								{ text: 'optimize优化', link: '/study/vue/vue2/compile/optimize' },
-								{ text: 'codegen代码生成', link: '/study/vue/vue2/compile/codegen' },
-							],
-							collapsible: true,
-							collapsed: true
-						},
-						{
-							text: '扩展',
-							items: [
-								{ text: '扩展', link: '/study/vue/vue2/expand/README' },
-								{ text: 'directive指令', link: '/study/vue/vue2/expand/directive' },
-								{ text: 'filter过滤器', link: '/study/vue/vue2/expand/filter' },
-								{ text: 'event事件处理', link: '/study/vue/vue2/expand/event' },
-								{ text: 'v-model', link: '/study/vue/vue2/expand/vmodel' },
-								{ text: '插槽', link: '/study/vue/vue2/expand/slot' },
-								{ text: 'Keep-Alive', link: '/study/vue/vue2/expand/keep-alive' },
-								{ text: 'Transition', link: '/study/vue/vue2/expand/transition' },
-								{ text: 'Transition-Group', link: '/study/vue/vue2/expand/transition-group' },
-								{ text: 'Vue.use插件机制', link: '/study/vue/vue2/expand/plugin' },
-							],
-							collapsible: true,
-							collapsed: true
-						},
-						{
-							text: 'Vue-Router',
-							items: [
-								{ text: '介绍', link: '/study/vue/vue2/router/README' },
-								{ text: '路由安装', link: '/study/vue/vue2/router/install' },
-								{ text: 'matcher介绍', link: '/study/vue/vue2/router/matcher' },
-								{ text: '路由切换', link: '/study/vue/vue2/router/change' },
-								{ text: '内置组件', link: '/study/vue/vue2/router/components' },
-								{ text: '路由hooks钩子函数', link: '/study/vue/vue2/router/hooks' },
-							],
-							collapsible: true,
-							collapsed: true
-						},
-						{
-							text: 'Vuex',
-							items: [
-								{ text: '介绍', link: '/study/vue/vue2/vuex/README' },
-								{ text: 'Vuex安装', link: '/study/vue/vue2/vuex/install' },
-								{ text: 'Vuex初始化', link: '/study/vue/vue2/vuex/init' },
-								{ text: 'Vuex辅助API', link: '/study/vue/vue2/vuex/utils' },
-								{ text: 'Store实例API', link: '/study/vue/vue2/vuex/api' },
-							],
-							collapsible: true, // 菜单是否为可折叠的
-							collapsed: true // 是否默认折叠 
-						},
-					],
-					collapsible: true, // 菜单是否为可折叠的
-					collapsed: true // 是否默认折叠 
-			},{
-					text: 'vue3学习笔记',
-					items: [
-						{
-							text: 'Vue-Router',
-							items: [
-								{ text: '介绍', link: '/study/vue/vue3/router/README' },
-							],
-							collapsible: true,
-							collapsed: true
-						},
-						{
-							text: 'Pinia',
-							items: [
-								{ text: '介绍', link: '/study/vue/vue3/pinia/README' },
-							],
-							collapsible: true, // 菜单是否为可折叠的
-							collapsed: true // 是否默认折叠 
-						},
-					],
-					collapsible: true, // 菜单是否为可折叠的
-					collapsed: true // 是否默认折叠 
-				},
-				{
-					text: 'javaScript',
-					items: [
-						{ text: 'ES6', link: '/study/javaScript/ES6' }
-					],
-					collapsible: true, // 菜单是否为可折叠的
-					collapsed: true // 是否默认折叠 
-				}, {
-					text: 'CSS学习笔记',
-					items: [
-						{ text: 'grid布局', link: '/study/css/grid' },
-						{ text: 'css3', link: '/study/css/css3' },
-					],
-					collapsible: true, // 菜单是否为可折叠的
-					collapsed: true // 是否默认折叠 
-				}, {
-					text: 'webpack学习笔记',
-					items: [
-						{ text: '追本溯源', link: '/study/webpack/source' },
-						{ text: '安装', link: '/study/webpack/install' },
-						{ text: '起步', link: '/study/webpack/start' },
-						{ text: '打包静态资源', link: '/study/webpack/static' },
-						{ text: 'webpack核心', link: '/study/webpack/core' },
-						{ text: 'webpack进阶', link: '/study/webpack/advanced' },
-						{ text: 'webpack配置案例', link: '/study/webpack/case' },
-						{ text: 'webpack性能优化', link: '/study/webpack/optimization' },
-						{ text: '编写自己的Loader', link: '/study/webpack/loader' },
-						{ text: '编写自己的Plugin', link: '/study/webpack/plugin' },
-					],
-					collapsible: true, // 菜单是否为可折叠的
-					collapsed: true // 是否默认折叠 
-				}, {
-					text: 'mapboxgl学习笔记',
-					items: [
-						{ text: 'mapboxgl', link: '/study/mapboxgl/API' },
-					],
-					collapsible: true, // 菜单是否为可折叠的
-					collapsed: true // 是否默认折叠 
-				}, {
-					text: '其他学习',
-					items: [
-						{ text: 'linux部署', link: '/study/other/linux' },
-						{ text: '抓包工具Whistle', link: '/study/other/Whistle' },
-						{ text: '服务器安装mysql', link: '/study/other/mysql' },
-						{ text: 'Git指令', link: '/study/other/git' },
-						{ text: 'rollup', link: '/study/other/rollup' },
-						{ text: '性能优化', link: '/study/other/performance-optimization' },
-					],
-					collapsible: true, // 菜单是否为可折叠的
-					collapsed: true // 是否默认折叠 
-				},
+				text: 'vue2学习笔记',
+				items: [
+					{ text: '介绍', link: '/study/vue/vue2/introduction/README' },
+					{ text: '源码目录设计和架构设计', link: '/study/vue/vue2/design/README' },
+					{
+						text: 'Rollup构建版本',
+						items: [
+							{ text: 'Rollup基础知识', link: '/study/vue/vue2/rollup/README' },
+							{ text: 'Vue中的Rollup构建', link: '/study/vue/vue2/rollup/vue' },
+						],
+						collapsible: true,
+						collapsed: true
+					},
+					{
+						text: '从入口到构造函数整体流程',
+						items: [
+							{ text: '整体流程', link: '/study/vue/vue2/entry/README' },
+							{ text: 'initGlobalAPI', link: '/study/vue/vue2/entry/global' },
+							{ text: 'initMixin流程', link: '/study/vue/vue2/entry/init' },
+							{ text: 'stateMixin流程', link: '/study/vue/vue2/entry/state' },
+							{ text: 'eventsMixin流程', link: '/study/vue/vue2/entry/events' },
+							{ text: 'lifecycleMixin流程', link: '/study/vue/vue2/entry/lifecycle' },
+							{ text: 'renderMixin流程', link: '/study/vue/vue2/entry/render' },
+						],
+						collapsible: true,
+						collapsed: true
+					},
+					{
+						text: '响应式原理',
+						items: [
+							{ text: '介绍', link: '/study/vue/vue2/reactive/README' },
+							{ text: '前置核心概念', link: '/study/vue/vue2/reactive/prepare' },
+							{ text: 'props处理', link: '/study/vue/vue2/reactive/props' },
+							{ text: 'methods处理', link: '/study/vue/vue2/reactive/methods' },
+							{ text: 'data处理', link: '/study/vue/vue2/reactive/data' },
+							{ text: 'computed处理', link: '/study/vue/vue2/reactive/computed' },
+							{ text: 'watch处理', link: '/study/vue/vue2/reactive/watch' },
+							{ text: '深入响应式原理', link: '/study/vue/vue2/reactive/reactive' },
+							{ text: '依赖收集', link: '/study/vue/vue2/reactive/dep' },
+							{ text: '派发更新', link: '/study/vue/vue2/reactive/notify' },
+							{ text: 'nextTick实现原理', link: '/study/vue/vue2/reactive/nexttick' },
+							{ text: '变化侦测注意事项', link: '/study/vue/vue2/reactive/problem' },
+							{ text: '变化侦测API实现', link: '/study/vue/vue2/reactive/api' },
+						],
+						collapsible: true,
+						collapsed: true
+					},
+					{
+						text: '虚拟DOM和VNode',
+						items: [
+							{ text: '虚拟DOM', link: '/study/vue/vue2/dom/README' },
+							{ text: 'VNode介绍', link: '/study/vue/vue2/dom/vnode' },
+							{ text: 'Diff算法', link: '/study/vue/vue2/dom/diff' },
+						],
+						collapsible: true,
+						collapsed: true
+					},
+					{
+						text: '组件化',
+						items: [
+							{ text: '介绍', link: '/study/vue/vue2/component/README' },
+							{ text: '$mount方法', link: '/study/vue/vue2/component/mount' },
+							{ text: 'render和renderProxy', link: '/study/vue/vue2/component/render' },
+							{ text: 'createElement', link: '/study/vue/vue2/component/createElement' },
+							{ text: 'createComponent', link: '/study/vue/vue2/component/createComponent' },
+							{ text: '合并策略', link: '/study/vue/vue2/component/merge' },
+							{ text: 'update和patch', link: '/study/vue/vue2/component/patch' },
+							{ text: '组件生命周期', link: '/study/vue/vue2/component/lifecycle' },
+							{ text: '组件注册', link: '/study/vue/vue2/component/register' },
+						],
+						collapsible: true,
+						collapsed: true
+					},
+					{
+						text: '编译原理',
+						items: [
+							{ text: '介绍', link: '/study/vue/vue2/compile/README' },
+							{ text: 'compileToFunctions', link: '/study/vue/vue2/compile/compileToFunctions' },
+							{ text: 'parse模板解析', link: '/study/vue/vue2/compile/parse' },
+							{ text: 'optimize优化', link: '/study/vue/vue2/compile/optimize' },
+							{ text: 'codegen代码生成', link: '/study/vue/vue2/compile/codegen' },
+						],
+						collapsible: true,
+						collapsed: true
+					},
+					{
+						text: '扩展',
+						items: [
+							{ text: '扩展', link: '/study/vue/vue2/expand/README' },
+							{ text: 'directive指令', link: '/study/vue/vue2/expand/directive' },
+							{ text: 'filter过滤器', link: '/study/vue/vue2/expand/filter' },
+							{ text: 'event事件处理', link: '/study/vue/vue2/expand/event' },
+							{ text: 'v-model', link: '/study/vue/vue2/expand/vmodel' },
+							{ text: '插槽', link: '/study/vue/vue2/expand/slot' },
+							{ text: 'Keep-Alive', link: '/study/vue/vue2/expand/keep-alive' },
+							{ text: 'Transition', link: '/study/vue/vue2/expand/transition' },
+							{ text: 'Transition-Group', link: '/study/vue/vue2/expand/transition-group' },
+							{ text: 'Vue.use插件机制', link: '/study/vue/vue2/expand/plugin' },
+						],
+						collapsible: true,
+						collapsed: true
+					},
+					{
+						text: 'Vue-Router',
+						items: [
+							{ text: '介绍', link: '/study/vue/vue2/router/README' },
+							{ text: '路由安装', link: '/study/vue/vue2/router/install' },
+							{ text: 'matcher介绍', link: '/study/vue/vue2/router/matcher' },
+							{ text: '路由切换', link: '/study/vue/vue2/router/change' },
+							{ text: '内置组件', link: '/study/vue/vue2/router/components' },
+							{ text: '路由hooks钩子函数', link: '/study/vue/vue2/router/hooks' },
+						],
+						collapsible: true,
+						collapsed: true
+					},
+					{
+						text: 'Vuex',
+						items: [
+							{ text: '介绍', link: '/study/vue/vue2/vuex/README' },
+							{ text: 'Vuex安装', link: '/study/vue/vue2/vuex/install' },
+							{ text: 'Vuex初始化', link: '/study/vue/vue2/vuex/init' },
+							{ text: 'Vuex辅助API', link: '/study/vue/vue2/vuex/utils' },
+							{ text: 'Store实例API', link: '/study/vue/vue2/vuex/api' },
+						],
+						collapsible: true, // 菜单是否为可折叠的
+						collapsed: true // 是否默认折叠 
+					},
+				],
+				collapsible: true, // 菜单是否为可折叠的
+				collapsed: true // 是否默认折叠 
+			}, {
+				text: 'java学习笔记',
+				items: [
+					{ text: '配置环境', link: '/study/java/base-config/java环境的安装以及IntelliJ IDEA安装' },
+					// {
+					// 	text: 'Rollup构建版本',
+					// 	items: [
+					// 		{ text: 'Rollup基础知识', link: '/study/vue/vue2/rollup/README' },
+					// 		{ text: 'Vue中的Rollup构建', link: '/study/vue/vue2/rollup/vue' },
+					// 	],
+					// 	collapsible: true,
+					// 	collapsed: true
+					// },
+				],
+				collapsible: true, // 菜单是否为可折叠的
+				collapsed: true // 是否默认折叠 
+			}, {
+				text: 'vue3学习笔记',
+				items: [
+					{
+						text: 'Vue-Router',
+						items: [
+							{ text: '介绍', link: '/study/vue/vue3/router/README' },
+						],
+						collapsible: true,
+						collapsed: true
+					},
+					{
+						text: 'Pinia',
+						items: [
+							{ text: '介绍', link: '/study/vue/vue3/pinia/README' },
+						],
+						collapsible: true, // 菜单是否为可折叠的
+						collapsed: true // 是否默认折叠 
+					},
+				],
+				collapsible: true, // 菜单是否为可折叠的
+				collapsed: true // 是否默认折叠 
+			},
+			{
+				text: 'javaScript',
+				items: [
+					{ text: 'ES6', link: '/study/javaScript/ES6' }
+				],
+				collapsible: true, // 菜单是否为可折叠的
+				collapsed: true // 是否默认折叠 
+			}, {
+				text: 'CSS学习笔记',
+				items: [
+					{ text: 'grid布局', link: '/study/css/grid' },
+					{ text: 'css3', link: '/study/css/css3' },
+				],
+				collapsible: true, // 菜单是否为可折叠的
+				collapsed: true // 是否默认折叠 
+			}, {
+				text: 'webpack学习笔记',
+				items: [
+					{ text: '追本溯源', link: '/study/webpack/source' },
+					{ text: '安装', link: '/study/webpack/install' },
+					{ text: '起步', link: '/study/webpack/start' },
+					{ text: '打包静态资源', link: '/study/webpack/static' },
+					{ text: 'webpack核心', link: '/study/webpack/core' },
+					{ text: 'webpack进阶', link: '/study/webpack/advanced' },
+					{ text: 'webpack配置案例', link: '/study/webpack/case' },
+					{ text: 'webpack性能优化', link: '/study/webpack/optimization' },
+					{ text: '编写自己的Loader', link: '/study/webpack/loader' },
+					{ text: '编写自己的Plugin', link: '/study/webpack/plugin' },
+				],
+				collapsible: true, // 菜单是否为可折叠的
+				collapsed: true // 是否默认折叠 
+			}, {
+				text: 'mapboxgl学习笔记',
+				items: [
+					{ text: 'mapboxgl', link: '/study/mapboxgl/API' },
+				],
+				collapsible: true, // 菜单是否为可折叠的
+				collapsed: true // 是否默认折叠 
+			}, {
+				text: '其他学习',
+				items: [
+					{ text: 'linux部署', link: '/study/other/linux' },
+					{ text: '抓包工具Whistle', link: '/study/other/Whistle' },
+					{ text: '服务器安装mysql', link: '/study/other/mysql' },
+					{ text: 'Git指令', link: '/study/other/git' },
+					{ text: 'rollup', link: '/study/other/rollup' },
+					{ text: '性能优化', link: '/study/other/performance-optimization' },
+				],
+				collapsible: true, // 菜单是否为可折叠的
+				collapsed: true // 是否默认折叠 
+			},
 			],
 			'/notes/': [
 				{
@@ -243,8 +259,8 @@ export default defineConfig({
 						{ text: '回流和重绘', link: '/notes/days/20230726-回流和重绘' },
 						{ text: 'TypeScript', link: '/notes/days/20230728-TypeScript' },
 						{ text: 'git不区分带小写问题', link: '/notes/days/20230928-git不区分大小写问题' },
-						
-						
+
+
 					],
 					collapsible: true, // 菜单是否为可折叠的
 					collapsed: true // 是否默认折叠 
@@ -270,8 +286,8 @@ export default defineConfig({
 			toc: { includeLevel: [1, 2] },
 
 			config: (md) => {
-			// use more markdown-it plugins!
-			md.use(require('markdown-it-xxx'))
+				// use more markdown-it plugins!
+				md.use(require('markdown-it-xxx'))
 			}
 		}
 	}
---
+++ docs/study/java/base-config/java环境的安装以及IntelliJ IDEA安装.md
@@ -0,0 +1,23 @@
+---
+sidebar: auto
+---
+
+## 1. java环境环境的安装与配置
++ 1.1 下载jdk
++ 1.2 配置环境变量
+[安装配置教程](https://blog.csdn.net/qq_50454266/article/details/119352105)
+
+## 2. IntelliJ IDEA的安装与破解方法
++ 2.1 打开[地址](https://hardbin.com/ipfs/bafybeia4nrbuvpfd6k7lkorzgjw3t6totaoko7gmvq5pyuhl2eloxnfiri/) 查看最新可破解的IDEA版本，并下载jebra.zip压缩包
+
+![java_1](/images/java/java_1.png)
+
++ 2.2 前往[IntelliJ IDEA官网](https://www.jetbrains.com/idea/download/?section=windows) 下载对应版本的IDEA 并安装（安装之后先不运行）
+
+![java_2](/images/java/java_2.png)
+
+![java_3](/images/java/java_3.png)
+
++ 2.3 将下载好的jebra.zip 压缩包解压, 找到jebra->script-> intall-all-users.vbs, 双击运行 选择ok;运行结束 提示 done;
++ 2.4 打开IDEA 选择激活码激活，选择步骤1中网站中复制的激活码 填充并激活
++ 2.5 IDEA 可以永久使用了
